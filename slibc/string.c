1dc8d15f32b4c1f7e36998c162fe756ce2847219
SeyranGit <barsegyan1806@gmail.com>
2025-05-27 00:31:41 +0300

    :)

--- slibc/string.c
+++ slibc/string.c
@@ -29,16 +29,13 @@ static inline i8 get_zero_index(slibc_word_t word) {
 slibc_size_t str_length(const i8 * const string) {
   const i8 *char_p;
   const slibc_word_t *longword_p;
-
   slibc_word_t longword;
-
   if (string == slibc_null) return (slibc_size_t)0;
   for (char_p = string; ((slibc_word_t)char_p & (SLIBC_WORD_SIZE - 1)) != 0; char_p++) {
     if (!(*char_p)) {
       return (slibc_size_t)(char_p - string);
     }
   }
-
   longword_p = (const slibc_word_t*)char_p;
   while (1) {
     longword = *longword_p++;
