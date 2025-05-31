b1ee9d05ed33cf7b44489bc6920b2d8332bdbafc
SeyranGit <barsegyan1806@gmail.com>
2025-05-31 19:54:03 +0300

    add SLIBC_WINAPI

--- include/mm.h
+++ include/mm.h
@@ -9,13 +9,19 @@
 
 #if defined(_WIN32)
 
+#ifdef _WIN64
+  #define SLIBC_WINAPI
+#else
+  #define SLIBC_WINAPI __stdcall
+#endif
+
 #define HEAP_ZERO_MEMORY  0x00000008
 #define HEAP_NO_SERIALIZE 0x00000001
 
 
-extern __stdcall i32 HeapFree(slibc_pointer, ui32, slibc_pointer);
-extern __stdcall slibc_pointer HeapAlloc(slibc_pointer, ui32, slibc_size_t);
-extern __stdcall slibc_pointer GetProcessHeap(void);
+extern SLIBC_WINAPI i32 HeapFree(slibc_pointer, ui32, slibc_pointer);
+extern SLIBC_WINAPI slibc_pointer HeapAlloc(slibc_pointer, ui32, slibc_size_t);
+extern SLIBC_WINAPI slibc_pointer GetProcessHeap(void);
 
 #endif
 
