9bef8f271448f9ce5c787fd6ad11fc1fe71b7383
SeyranGit <barsegyan1806@gmail.com>
2025-05-31 16:33:26 +0300

    add bad slibc_alloc implementation for linux

--- src/mm/alloc.c
+++ src/mm/alloc.c
@@ -20,7 +20,14 @@ slibc_pointer slibc_alloc(slibc_size_t size) {
 #elif defined(linux) && linux == 1
 
 
+#include <sys/mman.h>
+
+
 slibc_pointer slibc_alloc(slibc_size_t size) {
+  slibc_pointer m = mmap(slibc_null, size, PROT_READ | PROT_WRITE , MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (m) {
+    return m;
+  }
   return slibc_null;
 }
 
