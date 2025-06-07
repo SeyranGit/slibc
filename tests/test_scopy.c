749587313b35a6c2c24195e1939fbb282eb981a1
SeyranGit <barsegyan1806@gmail.com>
2025-06-07 23:16:45 +0300

    add test for scopy

--- tests/test_scopy.c
+++ tests/test_scopy.c
@@ -1,11 +1,12 @@
 /*
+ *
+ * 64-bit: gcc   tests/test_scopy.c src/string/scopy.c -Wall -Iinclude
+ * 64-bit: clang tests/test_scopy.c src/string/scopy.c -Weverything -Wno-unsafe-buffer-usage -Wno-cast-align -Iinclude
+ * 32-bit: clang tests/test_scopy.c src/string/scopy.c -Weverything -Wno-unsafe-buffer-usage -Wno-cast-align -Iinclude -m32
+ *
+ */
 
-64-bit: gcc   tests/test_scopy.c src/string/scopy.c -Wall -Iinclude
-64-bit: clang tests/test_scopy.c src/string/scopy.c -Weverything -Wno-unsafe-buffer-usage -Wno-cast-align -Iinclude
-32-bit: clang tests/test_scopy.c src/string/scopy.c -Weverything -Wno-unsafe-buffer-usage -Wno-cast-align -Iinclude -m32
-
-*/
-
+#include <types.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -33,14 +34,24 @@ static void t3(void) {
 }
 
 
+static void t4(void) {
+  char *s2 = "Hello, my name is Seyran!";
+  char s1[26]; // = {[0] = 'A'};
+  for (slibc_size_t i = 0; i < 26; i++) {
+    scopy(s1, s2 + i);
+    printf("%s\n", s1);
+  }
+}
+
+
 int main(void) {
   t1();
   t2();
   t3();
-
   t3();
   t2();
   t1();
+  t4();
   return 0;
 }
 
