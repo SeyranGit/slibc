90772721a0c585ef6b4881c680bb2ee352489f5d
SeyranGit <barsegyan1806@gmail.com>
2025-06-07 22:23:56 +0300

    update test

--- tests/test_mcopy.c
+++ tests/test_mcopy.c
@@ -5,27 +5,30 @@
 #include <mm.h>
 
 
+#define STR_SIZE 56
+
+
 static void t1(void) {
   i8 a[100];
-  i8 *s = "Hello, my name is Seyran and to day i love your and but not";
-  mcopy(a, s, 60);
+  i8 *s = "Hello, world! Hello, world! Hello, world! Hello, world!";
+  mcopy(a, s, STR_SIZE);
   printf("%s\n", a);
 }
 
 
 static void t2(void) {
   i8 a[100];
-  i8 s[] = "Hello, my name is Seyran and to day i love your and but not";
-  mcopy(a + 1, s, 60);
+  i8 s[] = "Hello, world! Hello, world! Hello, world! Hello, world!";
+  mcopy(a + 1, s, STR_SIZE);
   printf("%s\n", a + 1);
 }
 
 
 static void t4(void) {
   i8 a[100] = {0};
-  i8 s[] = "Hello, my name is Seyran and to day i love your and but not";
-  for (slibc_size_t i = 0; i < 60; i++) {
-    mcopy(a + 1, s + i, 60 - i);
+  i8 s[] = "Hello, world! Hello, world! Hello, world! Hello, world!";
+  for (slibc_size_t i = 0; i < STR_SIZE; i++) {
+    mcopy(a + 1, s + i, STR_SIZE - i);
     printf("%s\n", a + 1);
     mset(a, 0, 100);
   }
