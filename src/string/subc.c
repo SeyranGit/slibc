9f956de7fbf00875efdbc48b5fc95392e5ce44c5
SeyranGit <barsegyan1806@gmail.com>
2025-06-20 18:46:15 +0300

    subc v2

--- src/string/subc.c
+++ src/string/subc.c
@@ -1,12 +1,27 @@
 #include <types.h>
+#include <bytes.h>
 #include <string.h>
 
 
-i8 *subc(const i8 *string, i8 c) {
-  while (*string) {
-    if (*string++ == c) {
+i8 *subc(const i8 *string, i8 symbol) {
+  const SlibcWord *wp;
+  SlibcWord word;
+  while (*string && ((SlibcWord)string & (SLIBC_WORD_SIZE - 1))) {
+    if (*string++ == symbol) {
       return (i8*)(string - 1);
     }
   }
-  return SlibcNull;
+  for (wp = (SlibcWord*)string;; wp++) {
+    SlibcSize i;
+    word = *wp;
+    for (i = 0;
+         i < (has_null(word) ? iofzb(word) + 1 : SLIBC_WORD_SIZE);
+         i++) {
+      i8 c = ((i8*)&word)[i];
+      if (!c) return SlibcNull;
+      if (c == symbol) {
+        return (i8*)wp + i;
+      }
+    }
+  }
 }
