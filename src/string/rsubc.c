14f548bc2fff37df8d31efe2afe9bd5ae709563c
SeyranGit <barsegyan1806@gmail.com>
2025-06-20 22:34:59 +0300

    rename

--- src/string/rsubc.c
+++ src/string/rsubc.c
@@ -4,10 +4,10 @@
 
 i8 *rsubc(const i8 *string, i8 symbol) {
   const i8 *found = SlibcNull;
-  const i8 *next;
-  while ((next = subc(string, symbol)) != SlibcNull) {
-    found = next;
-    string = next + 1;
+  const i8 *p;
+  while ((p = subc(string, symbol)) != SlibcNull) {
+    found = p;
+    string = p + 1;
   }
   return (i8*)found;
 }
