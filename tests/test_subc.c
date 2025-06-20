e19ce10cdd72e648b7d1eb0402022153a10d28a4
SeyranGit <barsegyan1806@gmail.com>
2025-06-20 15:21:52 +0300

    use SlibcNull

--- tests/test_subc.c
+++ tests/test_subc.c
@@ -9,7 +9,7 @@ i32 main(void) {
   i8 target = 'T';
   const i8 *str = "Try not. Do, or do not. There is no try.";
   const i8 *result = str;
-  while ((result = subc(result, target)) != NULL) {
+  while ((result = subc(result, target)) != SlibcNull) {
     printf("Found '%c' starting at '%s'\n", target, result);
     ++result;
   }
