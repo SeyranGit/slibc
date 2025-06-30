81d14f00f089302793949749fdaa39367faad823
SeyranGit <barsegyan1806@gmail.com>
2025-07-01 02:07:52 +0300

    implemented both fast and slow implementation of srev

--- src/string/srev.c
+++ src/string/srev.c
@@ -12,3 +12,63 @@ i8 *srev(i8 * const string) {
   }
   return string;
 }
+
+
+#if 0
+/*
+ * Без оптимизации O3 данная версия работает на 0.8 секунды медленнее реализации выше.
+ *
+ * С оптимизацией O3 данная версия переворачивает
+ * строку длиной в 1.95 гб (каждый символ 1 байт) за 0.337000 секунды,
+ * что в 10 раз быстрее реализации выше без O3 (она выполняла ту же задачу за 3.1 секунды)
+ * и в 4.6 раза с O3 (1.4 секунды).
+ */
+
+
+static inline void swap(i8 **start, i8 **end) {
+  i8 tmp = **start;
+  *((*start)++) = **end;
+  *((*end)--) = tmp;
+}
+
+
+static inline SlibcWord rword(SlibcWord word) {
+  i8 *start = (i8*)(&word);
+  i8 *end = start + SLIBC_WORD_SIZE - 1;
+  while (start < end) {
+    swap(&start, &end);
+  }
+  return word;
+}
+
+
+#define diff(a, b) ((a + 1) - b)
+
+
+i8 *srev(i8 * const string) {
+  SlibcWord start = (SlibcWord)string;
+  SlibcWord end = start + slength(string) - 1;
+  while ((start % SLIBC_WORD_SIZE) && (start < end)) {
+    swap((i8**)&start, (i8**)&end);
+  }
+  if (diff(end, start) >= (SLIBC_WORD_SIZE * 2)) {
+    SlibcWord w0;
+    SlibcWord w1;
+    do {
+      w0 = *(SlibcWord*)(start);
+      w1 = *(SlibcWord*)(end - (SLIBC_WORD_SIZE - 1));
+
+      *(SlibcWord*)(start) = rword(w1);
+      *(SlibcWord*)(end - (SLIBC_WORD_SIZE - 1)) = rword(w0);
+
+      start += SLIBC_WORD_SIZE;
+      end -= SLIBC_WORD_SIZE;
+    } while (diff(end, start) >= (SLIBC_WORD_SIZE * 2));
+  }
+  while (start < end) {
+    swap((i8**)&start, (i8**)&end);
+  }
+  return string;
+}
+
+#endif
--- tests/test_srev.c
+++ tests/test_srev.c
@@ -1,14 +1,56 @@
-// clang tests/test_srev.c src/string/slength.c src/string/srev.c -Iinclude -Weverything -Wno-unsafe-buffer-usage -Wno-cast-align
+// clang tests/test_srev.c src/string/slength.c src/string/srev.c src/mm/free.c src/mm/alloc.c -Iinclude -Weverything -Wno-unsafe-buffer-usage -Wno-cast-align
 
 #include <stdio.h>
+#include <time.h>
+#include <sys/stat.h>
+
 #include <types.h>
 #include <string.h>
+#include <mm.h>
+
+
+static i8 *sff(void) {
+  struct _stat64i32 finfo;
+  i8 *buffer;
+  i8 *name = "a.txt";
+  FILE *file;
+  fopen_s(&file, name, "r");
+  if (!_stat(name, &finfo) || !file) {
+    buffer = (i8*)slibc_alloc((SlibcSize)finfo.st_size);
+    if (!buffer) {
+      return SlibcNull;
+    }
+    fread(buffer, sizeof(i8), (SlibcSize)finfo.st_size, file);
+    fclose(file);
+    return buffer;
+  }
+  return SlibcNull;
+}
 
 
 i32 main(void) {
+  clock_t start_time, end_time;
   i8 s1[] = "abcd";
   i8 s2[] = "abc";
+  i8 s3[] = "Hello, my name is Dambul! Hello!Hello, my name is Dambul! Hello!";
+  i8 s4[] = "Hello, my name is Dambul! Hello!";
+  i8 s5[] = "Hello, my name is Dambul!";
+  i8 s6[] = "HelloHelloHello!";
+  i8 *s7;
+
   printf("%s\n", srev(s1));
   printf("%s\n", srev(s2));
+  printf("%s\n", srev(s3));
+  printf("%s\n", srev(s4));
+  printf("%s\n", srev(s5));
+  printf("%s\n", srev(s6));
+
+
+  s7 = sff();
+  start_time = clock();
+  srev(s7);
+  end_time = clock();
+  printf("srev execution time: %f seconds\n", ((double) (end_time - start_time)) / CLOCKS_PER_SEC);
+  slibc_free((SlibcPointer)s7);
   return 0;
 }
